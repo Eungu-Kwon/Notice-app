323ce82cb0d9376fda3386beb60cdfbf3cb39e72
Eungu-Kwon <kiwi9084@gmail.com>
2019-07-31 14:26:35 +0900

    (7/31) Add Service

--- app/src/main/java/com/eungu/notice/MainActivity.java
+++ app/src/main/java/com/eungu/notice/MainActivity.java
@@ -1,9 +1,14 @@
 package com.eungu.notice;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,11 +19,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button setting_btn = findViewById(R.id.setting_btn);
+        Switch main_switch = (Switch)findViewById(R.id.main_switch);
+
         setting_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //TODO make setting
                 Toast.makeText(getApplicationContext(), "setting button clicked", Toast.LENGTH_SHORT).show();
             }
         });
+
+        main_switch.setChecked(isLaunchingService(getApplicationContext()));
+
+        main_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Intent main_service = new Intent(getApplicationContext(), NotiService.class);
+                if(isChecked)
+                    startService(main_service);
+                else
+                    //TODO make dialog
+                    stopService(main_service);
+            }
+        });
+    }
+
+    public Boolean isLaunchingService(Context mContext){
+
+        ActivityManager manager = (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
+
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (NotiService.class.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+
+        return  false;
     }
 }
--- app/src/main/java/com/eungu/notice/NotiService.java
+++ app/src/main/java/com/eungu/notice/NotiService.java
@@ -1,8 +1,14 @@
 package com.eungu.notice;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 
 public class NotiService extends Service {
     @Override
@@ -12,11 +18,33 @@ public class NotiService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "default");
+
+        builder.setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("서비스 실행중")
+                .setContentText("TEST")
+                .setPriority(NotificationCompat.PRIORITY_MIN)
+                .setNumber(0);
+
+        // 알림 표시
+        NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel nCh = new NotificationChannel("default", "기본 채널", NotificationManager.IMPORTANCE_LOW);
+            nCh.setShowBadge(false);
+            notificationManager.createNotificationChannel(nCh);
+        }
+
+        startForeground(1, builder.build());
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 }
